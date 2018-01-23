8642cc8b25eb0a2a3ee696bff3850e781ff23332
John Fallows <john.fallows@kaazing.com>
2018-01-23 12:48:51 -0800

    Support setting initial value of Last-Event-ID header via lastEventId query parameter

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 [![Build Status][build-status-image]][build-status]
 
+See official [Server-sent events specification](https://html.spec.whatwg.org/multipage/server-sent-events.html#server-sent-events).
+
+Also support for setting the initial value of `Last-Event-ID` via `lastEventId` query parameter. 
+
 [build-status-image]: https://travis-ci.org/reaktivity/nukleus-sse.spec.svg?branch=develop
 [build-status]: https://travis-ci.org/reaktivity/nukleus-sse.spec
--- src/test/java/org/reaktivity/specification/sse/ReconnectIT.java
+++ src/test/java/org/reaktivity/specification/sse/ReconnectIT.java
@@ -51,6 +51,18 @@ public class ReconnectIT
 
     @Test
     @Specification({
+        "${scripts}/request.header.and.parameter.last.event.id/request",
+        "${scripts}/request.header.and.parameter.last.event.id/response" })
+    @ScriptProperty("serverTransport \"nukleus://sse/streams/source\"")
+    public void shouldReconnectWithRequestHeaderAndParameterLastEventId() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
         "${scripts}/response.status.code.200/request",
         "${scripts}/response.status.code.200/response" })
     @ScriptProperty("serverTransport \"nukleus://sse/streams/source\"")
